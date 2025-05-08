81d249884758de72dbb03fd2260dbd3a08df83cf
Farrukh Ahmed Khan <77009721+farrukh-ahmed-khan@users.noreply.github.com>
2025-05-09 01:54:01 +0500

    sendemail done

--- src/lib/email/sendEmail.ts
+++ src/lib/email/sendEmail.ts
@@ -1,17 +1,41 @@
 import nodemailer from "nodemailer";
 
-export default async function sendEmail(to: string, subject: string, html: string) {
+export default async function sendEmail(
+  to: string,
+  subject: string,
+  html: string
+) {
+  // const transporter = nodemailer.createTransport({
+  //   service: "Gmail",
+  //   auth: {
+  //     user: process.env.EMAIL_USER,
+  //     pass: process.env.EMAIL_PASS,
+  //   },
+  // });
+
   const transporter = nodemailer.createTransport({
-    service: "Gmail",
+    host: process.env.MAIL_HOST,
+    port: parseInt(process.env.MAIL_PORT || "587"),
+    secure: false,
     auth: {
-      user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASS,
+      user: process.env.MAIL_USERNAME,
+      pass: process.env.MAIL_PASSWORD,
+    },
+    tls: {
+      ciphers: "SSLv3",
     },
   });
 
+  // await transporter.sendMail({
+  //   from: process.env.EMAIL_USER,
+  //   to,
+  //   subject,
+  //   html,
+  // });
+
   await transporter.sendMail({
-    from: process.env.EMAIL_USER,
-    to,
+    from: `"${process.env.MAIL_FROM_NAME}" <${process.env.MAIL_FROM_ADDRESS}>`,
+    to, 
     subject,
     html,
   });
