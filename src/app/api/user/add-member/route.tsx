3fd48abf070713b9558a13b9bbb098d07f463d75
Farrukh Ahmed Khan <77009721+farrukh-ahmed-khan@users.noreply.github.com>
2025-05-21 22:54:07 +0500

    remove unwant code

--- src/app/api/user/add-member/route.tsx
+++ src/app/api/user/add-member/route.tsx
@@ -8,47 +8,6 @@ import sendEmail from "@/lib/email/sendEmail";
 
 
 
-// export async function POST(req: Request) {
-//     try {
-//         await client;
-
-//         const authHeader = req.headers.get("authorization");
-//         if (!authHeader || !authHeader.startsWith("Bearer ")) {
-//             return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
-//         }
-
-//         const token = authHeader.split(" ")[1];
-//         const decoded = verifyToken(token);
-
-//         if (!decoded || decoded.role !== "home owner") {
-//             return NextResponse.json({ message: "Forbidden" }, { status: 403 });
-//         }
-
-//         const data = await req.json();
-//         const { firstname, lastname, email, phoneNumber, streetAddress, ownerId } = data;
-
-//         if (!firstname || !lastname || !email || !phoneNumber || !streetAddress || !ownerId) {
-//             return NextResponse.json({ message: "All fields are required" }, { status: 400 });
-//         }
-//         const newMember = new User({
-//             firstname,
-//             lastname,
-//             email,
-//             phoneNumber,
-//             streetAddress,
-//             role: "home member",
-//             ownerId: ownerId,
-//         });
-
-//         await newMember.save();
-
-//         return NextResponse.json({ message: "Member added successfully", member: newMember }, { status: 201 });
-
-//     } catch (error: any) {
-//         console.error("Add Member Error:", error);
-//         return NextResponse.json({ message: "Server Error", error: error.message }, { status: 500 });
-//     }
-// }
 
 
 export async function POST(req: Request) {
