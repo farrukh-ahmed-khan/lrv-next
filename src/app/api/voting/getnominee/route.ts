a2f0e6b8b0b769d2d2da8850bb4aebe38311bbd1
Farrukh Ahmed Khan <77009721+farrukh-ahmed-khan@users.noreply.github.com>
2025-05-22 01:50:05 +0500

    get all api for nominees done

--- src/app/api/voting/getnominee/route.ts
+++ src/app/api/voting/getnominee/route.ts
@@ -1,18 +1,19 @@
 import { client } from "@/lib/mongodb";
 import Newsletter from "@/lib/models/Newsletter";
 import { NextResponse } from "next/server";
+import Nominee from "@/lib/models/Nominee";
 
 
 export async function GET() {
   await client;
 
   try {
-    const newsletters = await Newsletter.find().sort({ createdAt: -1 });
-    return NextResponse.json({ newsletters }, { status: 200 });
+    const nominees = await Nominee.find().sort({ createdAt: -1 });
+    return NextResponse.json({ nominees }, { status: 200 });
   } catch (error: any) {
-    console.error("Fetch newsletters error:", error);
+    console.error("Fetch nominees error:", error);
     return NextResponse.json(
-      { message: "Failed to fetch newsletters", error: error.message },
+      { message: "Failed to fetch nominees", error: error.message },
       { status: 500 }
     );
   }
