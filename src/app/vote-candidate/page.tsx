71693ca70e221d030dbb83b7a11ebb940d585244
Farrukh Ahmed Khan <77009721+farrukh-ahmed-khan@users.noreply.github.com>
2025-06-13 23:03:46 +0500

    update

--- src/app/vote-candidate/page.tsx
+++ src/app/vote-candidate/page.tsx
@@ -3,24 +3,27 @@ import Header from "@/components/layout/Navbar";
 import HomeBanner from "@/components/ui/HomeBanner";
 import Footer from "@/components/layout/Footer";
 import NomineeVote from "@/components/ui/dashboard/VotingNominees";
+import ProtectedPage from "@/components/ProtectedPage";
 
 const Vote = () => {
     return (
         <>
-            <div className="home-wrapper">
-                <Header />
-                <section className="homeBanner-wrapper">
-                    <HomeBanner
-                        title="Vote Candidate"
+            <ProtectedPage allowedRoles={["home owner", "home member", "board member", "admin"]}>
+                <div className="home-wrapper">
+                    <Header />
+                    <section className="homeBanner-wrapper">
+                        <HomeBanner
+                            title="Vote Candidate"
 
-                    />
-                </section>
-                <>
-                    <NomineeVote />
-                </>
+                        />
+                    </section>
+                    <>
+                        <NomineeVote />
+                    </>
 
-                <Footer />
-            </div>
+                    <Footer />
+                </div>
+            </ProtectedPage>
         </>
     );
 };
